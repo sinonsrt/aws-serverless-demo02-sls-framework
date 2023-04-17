bfa278e91a566bd0b0ff2696a6176f22b7d7da12
Lucas Marchiori <sinonsrt@hotmail.com>
2023-04-17 17:03:21 -0300

    feat: sls-framwork

---
+++ run.sh
@@ -0,0 +1,24 @@
+# instalar o serverless
+yarn global add serverless
+
+#ci
+sls ou serverless 
+
+# iniciliar projeto
+sls
+
+#sempre fazer deploy antes de tudo para verificar se o ambiente está ok
+sls deploy
+
+#invocar aws
+sls invoke -f hello --log
+
+
+#invocar local
+sls invoke local -f hello --log
+
+#acompanhar logs
+sls logs -f hello --tail
+
+#remover
+sls remove
